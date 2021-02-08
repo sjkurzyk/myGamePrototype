89307d3ad65422f998d37b7db35bb944c31a85f3
sjkurzyk <58484703+sjkurzyk@users.noreply.github.com>
2021-02-08 14:56:22 +0000

    v0.05
    Added variables for parallax speed and background object ratios, font file and text to display controls, reduced width of background objects and altered the way they loop.

--- code0.js
+++ code0.js
@@ -14,6 +14,9 @@ gdjs.GameCode.GDbuildings_95farObjects3= [];
 gdjs.GameCode.GDbuildings_95closeObjects1= [];
 gdjs.GameCode.GDbuildings_95closeObjects2= [];
 gdjs.GameCode.GDbuildings_95closeObjects3= [];
+gdjs.GameCode.GDtrees_95foregroundObjects1= [];
+gdjs.GameCode.GDtrees_95foregroundObjects2= [];
+gdjs.GameCode.GDtrees_95foregroundObjects3= [];
 gdjs.GameCode.GDtreesObjects1= [];
 gdjs.GameCode.GDtreesObjects2= [];
 gdjs.GameCode.GDtreesObjects3= [];
@@ -26,6 +29,9 @@ gdjs.GameCode.GDfloor_95despawnerObjects3= [];
 gdjs.GameCode.GDfloor_95spawnerObjects1= [];
 gdjs.GameCode.GDfloor_95spawnerObjects2= [];
 gdjs.GameCode.GDfloor_95spawnerObjects3= [];
+gdjs.GameCode.GDscoreObjects1= [];
+gdjs.GameCode.GDscoreObjects2= [];
+gdjs.GameCode.GDscoreObjects3= [];
 
 gdjs.GameCode.conditionTrue_0 = {val:false};
 gdjs.GameCode.condition0IsTrue_0 = {val:false};
@@ -109,9 +115,30 @@ gdjs.GameCode.condition0IsTrue_0.val = false;
 {
 gdjs.GameCode.condition0IsTrue_0.val = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
 }if (gdjs.GameCode.condition0IsTrue_0.val) {
+gdjs.copyArray(runtimeScene.getObjects("buildings_close"), gdjs.GameCode.GDbuildings_95closeObjects1);
+gdjs.copyArray(runtimeScene.getObjects("buildings_far"), gdjs.GameCode.GDbuildings_95farObjects1);
+gdjs.copyArray(runtimeScene.getObjects("trees"), gdjs.GameCode.GDtreesObjects1);
+gdjs.copyArray(runtimeScene.getObjects("trees_foreground"), gdjs.GameCode.GDtrees_95foregroundObjects1);
 {runtimeScene.getVariables().get("speed").setNumber(5);
 }{gdjs.evtTools.sound.playMusicOnChannel(runtimeScene, "s-highway.mp3", 1, true, 20, 1);
 }{runtimeScene.getVariables().get("musicFlag").setNumber(1);
+}{runtimeScene.getVariables().getFromIndex(0).setNumber(5);
+}{runtimeScene.getVariables().getFromIndex(4).setNumber(40);
+}{runtimeScene.getVariables().getFromIndex(3).setNumber(8);
+}{runtimeScene.getVariables().getFromIndex(1).setNumber(1.1);
+}{runtimeScene.getVariables().getFromIndex(2).setNumber(0.9);
+}{for(var i = 0, len = gdjs.GameCode.GDtrees_95foregroundObjects1.length ;i < len;++i) {
+    gdjs.GameCode.GDtrees_95foregroundObjects1[i].setWidth(gdjs.evtTools.window.getGameResolutionWidth(runtimeScene) + 96);
+}
+}{for(var i = 0, len = gdjs.GameCode.GDtreesObjects1.length ;i < len;++i) {
+    gdjs.GameCode.GDtreesObjects1[i].setWidth(gdjs.evtTools.window.getGameResolutionWidth(runtimeScene) + 96);
+}
+}{for(var i = 0, len = gdjs.GameCode.GDbuildings_95closeObjects1.length ;i < len;++i) {
+    gdjs.GameCode.GDbuildings_95closeObjects1[i].setWidth(gdjs.evtTools.window.getGameResolutionWidth(runtimeScene) + 224);
+}
+}{for(var i = 0, len = gdjs.GameCode.GDbuildings_95farObjects1.length ;i < len;++i) {
+    gdjs.GameCode.GDbuildings_95farObjects1[i].setWidth(gdjs.evtTools.window.getGameResolutionWidth(runtimeScene) + 48);
+}
 }}
 
 }
@@ -192,6 +219,7 @@ gdjs.copyArray(runtimeScene.getObjects("buildings_far"), gdjs.GameCode.GDbuildin
 gdjs.copyArray(runtimeScene.getObjects("highway"), gdjs.GameCode.GDhighwayObjects1);
 gdjs.copyArray(runtimeScene.getObjects("highway_hole"), gdjs.GameCode.GDhighway_95holeObjects1);
 gdjs.copyArray(runtimeScene.getObjects("trees"), gdjs.GameCode.GDtreesObjects1);
+gdjs.copyArray(runtimeScene.getObjects("trees_foreground"), gdjs.GameCode.GDtrees_95foregroundObjects1);
 {for(var i = 0, len = gdjs.GameCode.GDhighwayObjects1.length ;i < len;++i) {
     gdjs.GameCode.GDhighwayObjects1[i].setX(gdjs.GameCode.GDhighwayObjects1[i].getX() - (gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().get("speed"))));
 }
@@ -199,13 +227,16 @@ gdjs.copyArray(runtimeScene.getObjects("trees"), gdjs.GameCode.GDtreesObjects1);
     gdjs.GameCode.GDhighway_95holeObjects1[i].setX(gdjs.GameCode.GDhighway_95holeObjects1[i].getX() - (gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().get("speed"))));
 }
 }{for(var i = 0, len = gdjs.GameCode.GDtreesObjects1.length ;i < len;++i) {
-    gdjs.GameCode.GDtreesObjects1[i].setX(gdjs.GameCode.GDtreesObjects1[i].getX() - (gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().get("speed")) * 0.9));
+    gdjs.GameCode.GDtreesObjects1[i].setX(gdjs.GameCode.GDtreesObjects1[i].getX() - (gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(0)) / gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(1))));
 }
 }{for(var i = 0, len = gdjs.GameCode.GDbuildings_95closeObjects1.length ;i < len;++i) {
-    gdjs.GameCode.GDbuildings_95closeObjects1[i].setX(gdjs.GameCode.GDbuildings_95closeObjects1[i].getX() - (gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().get("speed")) / 8));
+    gdjs.GameCode.GDbuildings_95closeObjects1[i].setX(gdjs.GameCode.GDbuildings_95closeObjects1[i].getX() - (gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(0)) / gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(3))));
 }
 }{for(var i = 0, len = gdjs.GameCode.GDbuildings_95farObjects1.length ;i < len;++i) {
-    gdjs.GameCode.GDbuildings_95farObjects1[i].setX(gdjs.GameCode.GDbuildings_95farObjects1[i].getX() - (gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().get("speed")) / 40));
+    gdjs.GameCode.GDbuildings_95farObjects1[i].setX(gdjs.GameCode.GDbuildings_95farObjects1[i].getX() - (gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(0)) / gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(4))));
+}
+}{for(var i = 0, len = gdjs.GameCode.GDtrees_95foregroundObjects1.length ;i < len;++i) {
+    gdjs.GameCode.GDtrees_95foregroundObjects1[i].setX(gdjs.GameCode.GDtrees_95foregroundObjects1[i].getX() - (gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(0)) / gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(2))));
 }
 }}
 
@@ -219,7 +250,7 @@ gdjs.copyArray(runtimeScene.getObjects("trees"), gdjs.GameCode.GDtreesObjects1);
 gdjs.GameCode.condition0IsTrue_0.val = false;
 {
 for(var i = 0, k = 0, l = gdjs.GameCode.GDtreesObjects1.length;i<l;++i) {
-    if ( gdjs.GameCode.GDtreesObjects1[i].getX() <= -(480) ) {
+    if ( gdjs.GameCode.GDtreesObjects1[i].getX() <= -(96) ) {
         gdjs.GameCode.condition0IsTrue_0.val = true;
         gdjs.GameCode.GDtreesObjects1[k] = gdjs.GameCode.GDtreesObjects1[i];
         ++k;
@@ -228,7 +259,30 @@ for(var i = 0, k = 0, l = gdjs.GameCode.GDtreesObjects1.length;i<l;++i) {
 gdjs.GameCode.GDtreesObjects1.length = k;}if (gdjs.GameCode.condition0IsTrue_0.val) {
 /* Reuse gdjs.GameCode.GDtreesObjects1 */
 {for(var i = 0, len = gdjs.GameCode.GDtreesObjects1.length ;i < len;++i) {
-    gdjs.GameCode.GDtreesObjects1[i].setX(0);
+    gdjs.GameCode.GDtreesObjects1[i].setX(gdjs.GameCode.GDtreesObjects1[i].getX() + (96));
+}
+}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("trees_foreground"), gdjs.GameCode.GDtrees_95foregroundObjects1);
+
+gdjs.GameCode.condition0IsTrue_0.val = false;
+{
+for(var i = 0, k = 0, l = gdjs.GameCode.GDtrees_95foregroundObjects1.length;i<l;++i) {
+    if ( gdjs.GameCode.GDtrees_95foregroundObjects1[i].getX() <= -(96) ) {
+        gdjs.GameCode.condition0IsTrue_0.val = true;
+        gdjs.GameCode.GDtrees_95foregroundObjects1[k] = gdjs.GameCode.GDtrees_95foregroundObjects1[i];
+        ++k;
+    }
+}
+gdjs.GameCode.GDtrees_95foregroundObjects1.length = k;}if (gdjs.GameCode.condition0IsTrue_0.val) {
+/* Reuse gdjs.GameCode.GDtrees_95foregroundObjects1 */
+{for(var i = 0, len = gdjs.GameCode.GDtrees_95foregroundObjects1.length ;i < len;++i) {
+    gdjs.GameCode.GDtrees_95foregroundObjects1[i].setX(gdjs.GameCode.GDtrees_95foregroundObjects1[i].getX() + (96));
 }
 }}
 
@@ -242,7 +296,7 @@ gdjs.copyArray(runtimeScene.getObjects("buildings_close"), gdjs.GameCode.GDbuild
 gdjs.GameCode.condition0IsTrue_0.val = false;
 {
 for(var i = 0, k = 0, l = gdjs.GameCode.GDbuildings_95closeObjects1.length;i<l;++i) {
-    if ( gdjs.GameCode.GDbuildings_95closeObjects1[i].getX() <= -(1120) ) {
+    if ( gdjs.GameCode.GDbuildings_95closeObjects1[i].getX() <= -(224) ) {
         gdjs.GameCode.condition0IsTrue_0.val = true;
         gdjs.GameCode.GDbuildings_95closeObjects1[k] = gdjs.GameCode.GDbuildings_95closeObjects1[i];
         ++k;
@@ -251,7 +305,7 @@ for(var i = 0, k = 0, l = gdjs.GameCode.GDbuildings_95closeObjects1.length;i<l;+
 gdjs.GameCode.GDbuildings_95closeObjects1.length = k;}if (gdjs.GameCode.condition0IsTrue_0.val) {
 /* Reuse gdjs.GameCode.GDbuildings_95closeObjects1 */
 {for(var i = 0, len = gdjs.GameCode.GDbuildings_95closeObjects1.length ;i < len;++i) {
-    gdjs.GameCode.GDbuildings_95closeObjects1[i].setX(0);
+    gdjs.GameCode.GDbuildings_95closeObjects1[i].setX(gdjs.GameCode.GDbuildings_95closeObjects1[i].getX() + (224));
 }
 }}
 
@@ -265,7 +319,7 @@ gdjs.copyArray(runtimeScene.getObjects("buildings_far"), gdjs.GameCode.GDbuildin
 gdjs.GameCode.condition0IsTrue_0.val = false;
 {
 for(var i = 0, k = 0, l = gdjs.GameCode.GDbuildings_95farObjects1.length;i<l;++i) {
-    if ( gdjs.GameCode.GDbuildings_95farObjects1[i].getX() <= -(240) ) {
+    if ( gdjs.GameCode.GDbuildings_95farObjects1[i].getX() <= -(48) ) {
         gdjs.GameCode.condition0IsTrue_0.val = true;
         gdjs.GameCode.GDbuildings_95farObjects1[k] = gdjs.GameCode.GDbuildings_95farObjects1[i];
         ++k;
@@ -274,7 +328,7 @@ for(var i = 0, k = 0, l = gdjs.GameCode.GDbuildings_95farObjects1.length;i<l;++i
 gdjs.GameCode.GDbuildings_95farObjects1.length = k;}if (gdjs.GameCode.condition0IsTrue_0.val) {
 /* Reuse gdjs.GameCode.GDbuildings_95farObjects1 */
 {for(var i = 0, len = gdjs.GameCode.GDbuildings_95farObjects1.length ;i < len;++i) {
-    gdjs.GameCode.GDbuildings_95farObjects1[i].setX(0);
+    gdjs.GameCode.GDbuildings_95farObjects1[i].setX(gdjs.GameCode.GDbuildings_95farObjects1[i].getX() + (48));
 }
 }}
 
@@ -321,7 +375,7 @@ gdjs.GameCode.condition0IsTrue_0.val = gdjs.evtTools.input.wasKeyReleased(runtim
 }if ( gdjs.GameCode.condition0IsTrue_0.val ) {
 {
 {gdjs.GameCode.conditionTrue_1 = gdjs.GameCode.condition1IsTrue_0;
-gdjs.GameCode.conditionTrue_1.val = runtimeScene.getOnceTriggers().triggerOnce(7091500);
+gdjs.GameCode.conditionTrue_1.val = runtimeScene.getOnceTriggers().triggerOnce(7349724);
 }
 }}
 if (gdjs.GameCode.condition1IsTrue_0.val) {
@@ -354,6 +408,9 @@ gdjs.GameCode.GDbuildings_95farObjects3.length = 0;
 gdjs.GameCode.GDbuildings_95closeObjects1.length = 0;
 gdjs.GameCode.GDbuildings_95closeObjects2.length = 0;
 gdjs.GameCode.GDbuildings_95closeObjects3.length = 0;
+gdjs.GameCode.GDtrees_95foregroundObjects1.length = 0;
+gdjs.GameCode.GDtrees_95foregroundObjects2.length = 0;
+gdjs.GameCode.GDtrees_95foregroundObjects3.length = 0;
 gdjs.GameCode.GDtreesObjects1.length = 0;
 gdjs.GameCode.GDtreesObjects2.length = 0;
 gdjs.GameCode.GDtreesObjects3.length = 0;
@@ -366,6 +423,9 @@ gdjs.GameCode.GDfloor_95despawnerObjects3.length = 0;
 gdjs.GameCode.GDfloor_95spawnerObjects1.length = 0;
 gdjs.GameCode.GDfloor_95spawnerObjects2.length = 0;
 gdjs.GameCode.GDfloor_95spawnerObjects3.length = 0;
+gdjs.GameCode.GDscoreObjects1.length = 0;
+gdjs.GameCode.GDscoreObjects2.length = 0;
+gdjs.GameCode.GDscoreObjects3.length = 0;
 
 gdjs.GameCode.eventsList1(runtimeScene);
 return;
